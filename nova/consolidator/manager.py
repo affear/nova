9c8969d6f519164f167c0ae55d1971dbd4167501
Lorenzo Affetti <affox90@gmail.com>
2015-02-09 12:47:46 +0100

    importing base consolidator class
    Change-Id: I615971e41304ce58aca82c10a13f38ecf93aca60

--- nova/consolidator/manager.py
+++ nova/consolidator/manager.py
@@ -1,4 +1,5 @@
 from oslo_config import cfg
+from oslo_utils import importutils
 import oslo_messaging as messaging
 from nova.openstack.common import log as logging
 from nova import manager
@@ -6,10 +7,10 @@ from nova.compute import rpcapi as compute_rpcapi
 from nova.openstack.common import periodic_task
 
 consolidator_opts = [
-	cfg.IntOpt(
-		'sample_opt',
-		default=0,
-		help='A sample option'
+	cfg.StrOpt(
+		'consolidator_class',
+		default='nova.consolidator.base.BaseConsolidator',
+		help='Full class name for consolidation algorithm'
 	),
 ]
 
@@ -24,10 +25,11 @@ class ConsolidatorManager(manager.Manager):
 
 	def __init__(self, *args, **kwargs):
 		self.compute_rpcapi = compute_rpcapi.ComputeAPI()
+		self.consolidator = importutils.import_class(CONF.consolidator_class)
 		super(ConsolidatorManager, self).__init__(service_name="consolidator", *args, **kwargs)
 
 	def log_sth(self, ctxt):
 		import random
 		strings = ['foo', 'bar', 'baz', 'boo', 'wof']
 
-		LOG.audit('Consolidator says: ' + random.choice(strings) + '!')
+		LOG.warning('Consolidator says: ' + random.choice(strings) + '!')
