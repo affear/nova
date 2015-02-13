68dc6083695aedc02951d60a69b90b8b244155c9
Lorenzo Affetti <affox90@gmail.com>
2015-02-13 18:22:35 +0100

    testok
    Change-Id: I8a15c52a88372eb24db38c14e7dcd97baeba3476

--- nova/tests/unit/consolidator/test_objects.py
+++ nova/tests/unit/consolidator/test_objects.py
@@ -2,11 +2,11 @@
 Unit Tests for nova.consolidator.objects
 """
 
-
+import mock
 from nova import context
 from nova import test
 from nova.consolidator import objects as cons_objects
-from nova.tests.api import fakes
+from nova.tests.unit.api.openstack import fakes
 from nova.tests.unit.objects import test_compute_node
 from nova.objects import instance, compute_node
 
@@ -24,20 +24,47 @@ class ConsolidatorObjectsTestCase(test.TestCase):
 	}
 
 	def setUp(self):
+		super(ConsolidatorObjectsTestCase, self).setUp()
 		self.ctxt = context.get_admin_context()
 		self.snapshot = cons_objects.Snapshot(self.ctxt)
+		self.cns = self._get_compute_nodes()
 
 	def _get_compute_nodes(self):
 		hosts = []
-		for h_id in self.architecture:
+		for k in self.architecture:
 			host = test_compute_node.fake_compute_node.copy()
-			host['id'] = h_id
-			hosts.append(host)
-		return [compute_node._from_db_object(self.ctxt, compute_node.ComputeNode(), h) for h in hosts]
+			host['id'] = k
+			host_obj = compute_node.ComputeNode._from_db_object(self.ctxt, compute_node.ComputeNode(), host)
+			hosts.append(host_obj)
+		return [cons_objects._ComputeNodeWrapper(self.ctxt, h) for h in hosts]
 
 	def _get_instances_by_host(self, host):
-		return [fakes.stub_instance(host=host) for i in xrange(self.architecture[host.id])]
+		id = str(host.id) + '{}'
+		instances = []
+		for i, cn in enumerate(self.cns):
+			instance = fakes.stub_instance(host=cn, id=id.format(i))
+			instances.append(instance)
+		return instances
+
+	def test_wrapper_method_missing(self):
+		cn = self.cns[0] # wrapped
+		self.assertRaises(AttributeError, lambda name: getattr(cn, name), 'not_existent_attribute')
+		self.assertEquals(cn.id, cn.cn.id)
+		self.assertEquals(cn.vcpus, cn.cn.vcpus)
+		self.assertEquals(cn.memory_mb, cn.cn.memory_mb)
+		self.assertEquals(cn.local_gb, cn.cn.local_gb)
+		val = 'doesnt_matter'
+		cn._get_instances = mock.Mock(return_value=val)
+		self.assertEquals(cn.instances, val)
 
 	def test_returned_compute_nodes_from_snapshot(self):
 		self.stubs.Set(self.snapshot, '_get_compute_nodes', self._get_compute_nodes)
-		self.assertItemsEqual(self._get_compute_nodes(), self.snapshot.nodes)
+		cfr_id_expected = [cn.id for cn in self.cns]
+		cfr_id_real = [cn.id for cn in self.snapshot.nodes]
+		self.assertItemsEqual(cfr_id_expected, cfr_id_real)
+
+	def test_returned_instances_from_cn(self):
+		for node in self.cns:
+			instances = self._get_instances_by_host(node.cn)
+			self.stubs.Set(node, '_get_instances', lambda: instances)
+			self.assertItemsEqual(node.instances, instances)
