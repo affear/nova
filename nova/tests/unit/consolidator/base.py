1de17b8f11dd36a897481833da49d1a98a390fb0
Lorenzo Affetti <affox90@gmail.com>
2015-02-17 17:10:33 +0100

    return correct snapshot
    Change-Id: Ie8c8e0359094a3f9b0e4e4d22a687f83ac805f6d

--- nova/tests/unit/consolidator/base.py
+++ nova/tests/unit/consolidator/base.py
@@ -5,17 +5,18 @@ from nova.consolidator import objects as cons_objects
 from nova.tests.unit.api.openstack import fakes
 from nova.tests.unit.objects import test_compute_node
 from nova.objects import instance, compute_node
+from nova.compute import power_state
 
 class TestCaseWithSnapshot(test.TestCase):
 
 	# mapping host_id -> number of instances
 	architecture = {
-		0: 1,
-		1: 4,
-		2: 0,
-		3: 10,
-		4: 7,
-		5: 3,
+		0: 10,
+		1: 40,
+		2: 19,
+		3: 100,
+		4: 75,
+		5: 33,
 		6: 0
 	}
 
@@ -30,9 +31,14 @@ class TestCaseWithSnapshot(test.TestCase):
 		snapshot = cons_objects.Snapshot(self.ctxt)
 		nodes = self.cns[:no_nodes]
 		snapshot._get_compute_nodes = mock.Mock(return_value=nodes)
+		all_instances = []
 		for node in snapshot.nodes:
 			instances = self._get_instances_by_host(node.cn)
+			all_instances.extend(instances)
 			node._get_instances = mock.Mock(return_value=instances)
+
+		self.snapshot._get_instances = mock.Mock(return_value=all_instances)
+
 		return snapshot
 
 	def _get_compute_nodes(self):
@@ -48,7 +54,7 @@ class TestCaseWithSnapshot(test.TestCase):
 		id = str(host.id) + '{}'
 		instances = []
 		for i in xrange(self.architecture[host.id]):
-			inst = fakes.stub_instance(host=host, id=id.format(i))
+			inst = fakes.stub_instance(host=host, power_state=power_state.RUNNING, id=id.format(i))
 			inst_obj = instance.Instance._from_db_object(self.ctxt, instance.Instance(), inst)
 			instances.append(inst_obj)
 		return instances
